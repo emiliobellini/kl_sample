3fd60ac7552e5d6283f6dc4a03e894abce853c78
Emilio Bellini <emilio.bellini@gmail.com>
2018-09-05 12:12:10 +0100

    Started map calculation

--- kl_sample/prep_fourier.py
+++ kl_sample/prep_fourier.py
@@ -6,12 +6,15 @@ the data will be stored in the repository.
 
 """
 
+import sys
 import numpy as np
-from astropy.io import fits
-from astropy import wcs
-import io
 import matplotlib
 import matplotlib.pyplot as plt
+from astropy.io import fits
+from astropy import wcs
+import settings as set
+import io
+
 
 def prep_fourier(args):
     """ Prepare data in fourier space.
@@ -24,19 +27,30 @@ def prep_fourier(args):
 
     """
 
-    dim_orig = 1 #Original pixels dimension in arcsec
-    dim_new = 120 #New pixels dimension in arcsec
+    # Local variables
+    dim_orig = 1 # Original pixel dimension of the mask in arcsec
+    dim_new = 120 # New pixel dimension in arcsec
+    n_avg = 5 # Number of new pixels used to average the multiplicative correction to the shear
+
+
+    # Infer dereived quantities
+    fields = ['W'+str(x+1) for x in range(4)]
+    z_bins = np.array([[set.Z_BINS[n], set.Z_BINS[n+1]] for n in np.arange(len(set.Z_BINS)-1)])
     dim_ratio = int(np.round(float(dim_new)/float(dim_orig)))
 
 
     # Define absolute paths and check the existence of each required file
     path = {}
-    for count in range(4):
-        field = 'W'+str(count+1)
-        path['input'] = io.path_exists_or_error(args.input_folder)
-        path['mask_'+field] = io.path_exists_or_error(path['input']+'/'+field+'.16bit.small.reg2.fits.gz')
+    path['input'] = io.path_exists_or_error(args.input_folder)
+    for f in fields:
+        path['mask_'+f] = io.path_exists_or_error(path['input']+'/'+f+'.16bit.small.reg2.fits.gz')
     path['output'] = io.path_exists_or_error(args.input_folder)
 
+    # Read galaxy catalogue
+    gals = io.read_from_fits(path['input']+'/data.fits', 'data')
+
+
+    # Plotting function
     def plot_and_save(table, fname):
         plt.imshow(table,interpolation='nearest')
         plt.colorbar()
@@ -45,68 +59,118 @@ def prep_fourier(args):
         return
 
 
-    for count in range(4):
-
-        field = 'W'+str(count+1)
-        print 'Field ' + field + ':'
-
-        # Read and plot original data
-        data = io.read_from_fits(path['mask_'+field], 'primary').astype(int)
-        print '----> Original shape: ', data.shape
-
-        # Convert to boolean and plot
-        data = (1-np.array(data, dtype=bool)).astype(bool)
-
-        # Calculate new dimensions
-        div1, mod1 = np.divmod(data.shape[0], dim_ratio)
-        div2, mod2 = np.divmod(data.shape[1], dim_ratio)
-        if mod1 == 0:
-            x1 = div1
-        else:
-            x1 = div1+1
-        if mod2 == 0:
-            x2 = div2
-        else:
-            x2 = div2+1
-        start1 = int(np.round((x1*dim_ratio-data.shape[0])/2.))
-        start2 = int(np.round((x2*dim_ratio-data.shape[1])/2.))
-        end1 = start1+data.shape[0]
-        end2 = start2+data.shape[1]
-
-        # Add borders to array and plot
-        data_ext = np.zeros((x1*dim_ratio, x2*dim_ratio), dtype=bool)
-        data_ext[start1:end1,start2:end2] = data
-        plot_and_save(data_ext, path['output']+'/'+field+'_orig.pdf')
-        print '----> Extended shape: ', data_ext.shape
-
-        # Calculate new array and plot
-        data_new = np.zeros((x1, x2))
-        for count1 in range(x1):
-            for count2 in range(x2):
-                s1 = count1*dim_ratio
-                s2 = count2*dim_ratio
-                new_pixel = data_ext[s1:s1+dim_ratio,s2:s2+dim_ratio].astype(float)
-                data_new[count1,count2] = np.average(new_pixel)
-        plot_and_save(data_new, path['output']+'/'+field+'_new.pdf')
-        print '----> New shape: ', (x1, x2)
-        add1 = float(x1)*dim_ratio/data.shape[0]-1.
-        add2 = float(x2)*dim_ratio/data.shape[1]-1.
-        print '----> Pixels added: '+'({0:5.2%}, '.format(add1)+'{0:5.2%})'.format(add2)
+    # Function that filters the galaxies
+    def filter_gals(gals, field, z_bin):
+        sel = set.get_mask(gals, z_bin[0], z_bin[1])
+        sel = np.array([x[:2] in f for x in gals['id']])*sel
+        return sel
 
 
+    # Main loop to scan over the fields TODO: replace ['W3'] with fields
+    for f in ['W3']:
 
-        header = io.read_header_from_fits(path['mask_'+field], 'primary')
-        # Create a new WCS object
-        w = wcs.WCS(naxis=2)
-        # Write header
-        w.wcs.crpix = np.array([start1+header['CRPIX1'], start2+header['CRPIX2']])/dim_ratio
-        w.wcs.cdelt = np.array([header['CD1_1'], header['CD2_2']])*dim_ratio
-        w.wcs.crval = np.array([header['CRVAL1'], header['CRVAL2']])
-        w.wcs.ctype = [header['CTYPE1'], header['CTYPE2']]
-        header = w.to_header()
-        # Write to file
-        fname = path['input']+'/'+field+'_mask.fits'
-        io.write_to_fits(fname, data_new, field+'_mask', header=header)
+        # Calculating the mask
+        print 'Calculating mask for field ' + f + ':'
+        sys.stdout.flush()
 
+
+        # # Read original mask
+        # mask_orig = io.read_from_fits(path['mask_'+f], 'primary').astype(int)
+        # print '----> Original shape: ', mask_orig.shape
+        # sys.stdout.flush()
+        #
+        # # Convert the mask to boolean
+        # mask_orig = (1-np.array(mask_orig, dtype=bool)).astype(bool)
+        #
+        # # Calculate new dimensions
+        # div1, mod1 = np.divmod(mask_orig.shape[0], dim_ratio)
+        # div2, mod2 = np.divmod(mask_orig.shape[1], dim_ratio)
+        # if mod1 == 0:
+        #     x1 = div1
+        # else:
+        #     x1 = div1+1
+        # if mod2 == 0:
+        #     x2 = div2
+        # else:
+        #     x2 = div2+1
+        # start1 = int(np.round((x1*dim_ratio-mask_orig.shape[0])/2.))
+        # start2 = int(np.round((x2*dim_ratio-mask_orig.shape[1])/2.))
+        # end1 = start1+mask_orig.shape[0]
+        # end2 = start2+mask_orig.shape[1]
+        #
+        # # Add borders to the mask
+        # mask_orig_ext = np.zeros((x1*dim_ratio, x2*dim_ratio), dtype=bool)
+        # mask_orig_ext[start1:end1,start2:end2] = mask_orig
+        # plot_and_save(mask_orig_ext, path['output']+'/'+f+'_orig.pdf')
+        # print '----> Extended shape: ', mask_orig_ext.shape
+        # sys.stdout.flush()
+        #
+        # # Calculate new mask
+        # mask = np.zeros((x1, x2))
+        # for count1 in range(x1):
+        #     for count2 in range(x2):
+        #         s1 = count1*dim_ratio
+        #         s2 = count2*dim_ratio
+        #         new_pix = mask_orig_ext[s1:s1+dim_ratio,s2:s2+dim_ratio].astype(float)
+        #         mask[count1,count2] = np.average(new_pix)
+        # plot_and_save(mask, path['output']+'/'+f+'_new.pdf')
+        # print '----> New shape: ', (x1, x2)
+        # sys.stdout.flush()
+        # add1 = float(x1)*dim_ratio/mask_orig.shape[0]-1.
+        # add2 = float(x2)*dim_ratio/mask_orig.shape[1]-1.
+        # print '----> Pixels added: '+'({0:5.2%}, '.format(add1)+'{0:5.2%})'.format(add2)
+        # sys.stdout.flush()
+        #
+        # # Create a WCS object and save it to file
+        # hd = io.read_header_from_fits(path['mask_'+f], 'primary')
+        # # Create a new WCS object
+        # w = wcs.WCS(naxis=2)
+        # # Write header
+        # w.wcs.crpix = np.array([start1+hd['CRPIX1'], start2+hd['CRPIX2']])/dim_ratio
+        # w.wcs.cdelt = np.array([hd['CD1_1'], hd['CD2_2']])*dim_ratio
+        # w.wcs.crval = np.array([hd['CRVAL1'], hd['CRVAL2']])
+        # w.wcs.ctype = [hd['CTYPE1'], hd['CTYPE2']]
+        # hd = w.to_header()
+        # # Write to file
+        # fname = path['input']+'/'+f+'_mask.fits'
+        # io.write_to_fits(fname, mask, f+'_mask', header=hd)
+
+
+        # TODO: remove this and uncomment the previous lines
+        hd = io.read_header_from_fits(path['input']+'/'+f+'_mask.fits', f+'_mask')
+        mask = io.read_from_fits(path['input']+'/'+f+'_mask.fits', f+'_mask')
+        w = wcs.WCS(hd)
+        z_bins = np.array([z_bins[0]]) #TODO: remove this
+
+
+        # Calculating the map
+        print 'Calculating map for field ' + f + ':'
+        sys.stdout.flush()
+
+        # Second loop to divide galaxies in redshift bins
+        for z_bin in z_bins:
+
+            # Filter galaxies
+            gals_f = gals[filter_gals(gals, f, z_bin)]
+
+            # World position
+            pos_w = np.array([[gals_f['ALPHA_J2000'][x],gals_f['DELTA_J2000'][x]] for x in range(len(gals_f))])
+            # Pixel position
+            pos_pix = w.wcs_world2pix(pos_w, 1).astype(int)
+            print pos_w
+            print pos_pix
+
+
+
+    # # print set.get_mask(gals,1.,2.)
+    # id = gals['id'][0]
+    # m = gals['m'][0]
+    # e1 = gals['e1'][0]
+    # e2 = gals['e2'][0]
+    # c2 = gals['c2'][0]
+    # zb = gals['Z_B'][0]
+    # p1 = gals['ALPHA_J2000'][0]
+    # p2 = gals['DELTA_J2000'][0]
+    # weight = gals['weight'][0]
 
     print 'Success!!'

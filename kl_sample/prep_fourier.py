17a811a309182d413935bca4b5498bd4a2fee83f
Emilio Bellini <emilio.bellini@gmail.com>
2018-10-09 14:19:56 +0100

    Modified a bit the structure to make available functions to other modules

--- kl_sample/prep_fourier.py
+++ kl_sample/prep_fourier.py
@@ -21,6 +21,7 @@ from astropy.io import fits
 from astropy import wcs
 import settings as set
 import io
+import prep_fourier_tools as tools
 
 
 def prep_fourier(args):
@@ -327,7 +328,7 @@ def prep_fourier(args):
             sys.stdout.flush()
 
 
-            # Save to file the map
+            # Save to file the mask
             name = 'MASK_{}'.format(f)
             warning = io.write_to_fits(path['mask_'+f], mask, name, header=hd, type='image') or warning
 
@@ -720,42 +721,8 @@ def prep_fourier(args):
                         sys.stdout.flush()
                         return True
 
-                # Create arrays for the two shears
-                map_1 = np.zeros(mask.shape)
-                map_2 = np.zeros(mask.shape)
-
-                # Get World position of each galaxy
-                pos = zip(cat['ALPHA_J2000'],cat['DELTA_J2000'])
-                # Calculate Pixel position of each galaxy
-                pos = w.wcs_world2pix(pos, 1).astype(int)
-                pos = np.flip(pos,axis=1) #Need to invert the columns
-
-
-                # Perform lex sort and get the sorted indices
-                sorted_idx = np.lexsort(pos.T)
-                sorted_pos =  pos[sorted_idx,:]
-                # Differentiation along rows for sorted array
-                diff_pos = np.diff(sorted_pos,axis=0)
-                diff_pos = np.append([True],np.any(diff_pos!=0,1),0)
-                # Get unique sorted labels
-                sorted_labels = diff_pos.cumsum(0)-1
-                # Get labels
-                labels = np.zeros_like(sorted_idx)
-                labels[sorted_idx] = sorted_labels
-                # Get unique indices
-                unq_idx  = sorted_idx[diff_pos]
-                # Get unique pos's and ellipticities
-                pos_unique = pos[unq_idx,:]
-                w_at_pos = np.bincount(labels, weights=cat['weight'])
-                g1_at_pos = np.bincount(labels, weights=cat['e1']*cat['weight'])/w_at_pos
-                g2_at_pos = np.bincount(labels, weights=cat['e2']*cat['weight'])/w_at_pos
-                # Create the maps
-                map_1[pos_unique[:,0],pos_unique[:,1]] = g1_at_pos
-                map_2[pos_unique[:,0],pos_unique[:,1]] = g2_at_pos
-
-
-                print '----> Empty pixels: {0:5.2%}'.format(1.-np.array([mask[tuple(x)] for x in pos_unique]).sum()/mask.flatten().sum())
-                sys.stdout.flush()
+                # Get map
+                map_1, map_2 = tools.get_map(w, mask, cat)
 
 
                 # Save to file the map
---
+++ kl_sample/prep_fourier_tools.py
@@ -0,0 +1,65 @@
+"""
+
+This module contains the tools to prepare
+data in fourier space.
+
+Functions:
+ - get_map(w, mask, cat)
+
+"""
+
+import sys
+import numpy as np
+
+
+def get_map(w, mask, cat):
+    """ Generate a map from a catalogue, a mask
+        and a WCS object.
+
+    Args:
+        w: WCS object.
+        mask: array with mask.
+        cat: catalogue of objects.
+
+    Returns:
+        map: array with map.
+
+    """
+
+    # Create arrays for the two shears
+    map_1 = np.zeros(mask.shape)
+    map_2 = np.zeros(mask.shape)
+
+    # Get World position of each galaxy
+    pos = zip(cat['ALPHA_J2000'],cat['DELTA_J2000'])
+    # Calculate Pixel position of each galaxy
+    pos = w.wcs_world2pix(pos, 1).astype(int)
+    pos = np.flip(pos,axis=1) #Need to invert the columns
+
+    # Perform lex sort and get the sorted indices
+    sorted_idx = np.lexsort(pos.T)
+    sorted_pos =  pos[sorted_idx,:]
+    # Differentiation along rows for sorted array
+    diff_pos = np.diff(sorted_pos,axis=0)
+    diff_pos = np.append([True],np.any(diff_pos!=0,1),0)
+    # Get unique sorted labels
+    sorted_labels = diff_pos.cumsum(0)-1
+    # Get labels
+    labels = np.zeros_like(sorted_idx)
+    labels[sorted_idx] = sorted_labels
+    # Get unique indices
+    unq_idx  = sorted_idx[diff_pos]
+    # Get unique pos's and ellipticities
+    pos_unique = pos[unq_idx,:]
+    w_at_pos = np.bincount(labels, weights=cat['weight'])
+    g1_at_pos = np.bincount(labels, weights=cat['e1']*cat['weight'])/w_at_pos
+    g2_at_pos = np.bincount(labels, weights=cat['e2']*cat['weight'])/w_at_pos
+    # Create the maps
+    map_1[pos_unique[:,0],pos_unique[:,1]] = g1_at_pos
+    map_2[pos_unique[:,0],pos_unique[:,1]] = g2_at_pos
+
+    empty = 1.-np.array([mask[tuple(x)] for x in pos_unique]).sum()/mask.flatten().sum()
+    print '----> Empty pixels: {0:5.2%}'.format(empty)
+    sys.stdout.flush()
+
+    return map_1, map_2
